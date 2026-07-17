91559b423cd7360030ce8ae0cfcdd37ab6e8e717
Rhimkhan <rhim78975730@gmail.com>
2026-07-18 00:08:43 +0530

    feat: add structured logging utility

---
+++ object ddetection yolo/utils/logger.py
@@ -0,0 +1,19 @@
+﻿import logging
+import sys
+
+
+def setup_logger(name: str = "object_detection_api") -> logging.Logger:
+    """Create and return a configured logger that writes to stdout.
+
+    Prevents duplicate handlers if the logger is fetched multiple times.
+    """
+    logger = logging.getLogger(name)
+    if not logger.handlers:
+        logger.setLevel(logging.INFO)
+        handler = logging.StreamHandler(sys.stdout)
+        formatter = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+    return logger
